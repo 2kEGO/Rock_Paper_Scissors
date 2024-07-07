a98e05d83a20021f435815ca9fea98fe5918124e
Long Nguyen <barnonguyen@gmail.com>
2024-07-06 21:27:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from random import randint
+
+def base():
+    t = ['Paper', 'Scissors', 'Rock']
+
+    compute =  t[randint(0,2)]
+
+    user_input = input(str('Enter your choice'))
+
+    if user_input not in t:
+        print('Wrong answer')
+    elif user_input == compute:
+        print('Tie')
+    elif user_input == 'Rock':
+        if compute == 'Paper':
+            print('You lose')
+        elif compute == 'Scissors':
+            print('You win')
+    elif user_input == 'Paper':
+        if compute == 'Rock':
+            print('You lose')
+        elif compute == 'Scissors':
+            print('You win')
+    elif user_input == 'Paper':
+        if compute == 'Rock':
+            print('You win')
+        elif compute == 'Scissors':
+            print('You lose')
+
+base()
